990622ef25225bc303bb5bc8f5e1d46ca4d55f4a
Shant Dashjian <shantgdashjian@gmail.com>
2024-05-11 22:15:38 +0400

    Fix name

--- kubernetes/platform/development/create-cluster.sh
+++ kubernetes/platform/development/create-cluster.sh
@@ -2,7 +2,7 @@
 
 echo "\n📦 Initializing Kubernetes cluster...\n"
 
-minikube start --cpus 2 --memory 4g --driver docker --profile bookshop
+minikube start --cpus 2 --memory 3g --driver docker --profile bookshop
 
 echo "\n🔌 Enabling NGINX Ingress Controller...\n"
 
